39a39ce5c40586bef48f3ffc7f979aa4b6a23956
Rishi181988 <124787348+Rishi181988@users.noreply.github.com>
2025-04-25 21:20:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title(' Machine Learning App')
 
 st.write('This app builds a machine learning model!')
+
+df = pd.read_csv("https://raw.githubusercontent.com/Rishi181988/SL-machinelearning/refs/heads/master/penguins_cleaned.csv")
+df
