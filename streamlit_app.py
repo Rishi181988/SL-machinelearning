0c18a84ee4b471d20701c1c2e40d95c789512d00
Rishi181988 <124787348+Rishi181988@users.noreply.github.com>
2025-04-25 21:23:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title(' Machine Learning App')
 
 st.write('This app builds a machine learning model!')
 
-df = pd.read_csv("https://raw.githubusercontent.com/Rishi181988/SL-machinelearning/refs/heads/master/penguins_cleaned.csv")
-df
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv("https://raw.githubusercontent.com/Rishi181988/SL-machinelearning/refs/heads/master/penguins_cleaned.csv")
+  df
