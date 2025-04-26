ad8b23e0c0127a24dd503928a3d5a1efe56d1823
Rishi181988 <124787348+Rishi181988@users.noreply.github.com>
2025-04-26 21:27:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,12 @@ with st.sidebar:
           'bill_depth_mm': bill_depth_mm,
           'flipper_length_mm': flipper_length_mm,
           'body_mass_g': body_mass_g,
-          'gender': gender}
+          'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, x], axis = 0)
 
 with st.expander('Input features'):
-  st.write('**Input penguins')
+  st.write('**Input penguin')
   input_df
   st.write('**Combined penguins data**')
   input_penguins
